f05073a3f28b6209fa43388bc496a7b2a77d7c91
Zhuk Ivan <zhukekIvan@yandex.ru>
2022-02-26 19:54:48 +0300

    webpack-ready

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const popupProfileForm = document.querySelector('#profile-form');
 const popupAddForm = document.querySelector('#addForm');
 const closeButtons = document.querySelectorAll('.popup__close-button');
+const forms = Array.from(document.querySelectorAll('.popup__form'));
 
 import { editProfile } from "./components/modal.js";
 import { addCard } from "./components/card.js";
 import { closePopup } from "./components/util.js";
+import { setValidation } from "./components/validate.js";
+import './pages/index.css'
 
 export const validationConfig = {
   submitButton: 'popup__save-button',
@@ -13,6 +16,10 @@ export const validationConfig = {
   inputErrorClass: 'popup__input_error',
   errorClass: 'popup__input-error_active'
 }
+
+forms.forEach(function(form) {
+  setValidation (form, validationConfig)
+}) /* Добавляет валидацию на все формы */
 
 closeButtons.forEach(function(button) {
   button.addEventListener('click', closePopup);
