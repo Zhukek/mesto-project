636ed0c07f2a62aaf8282935e20a0eaa058ad500
Zhuk Ivan <zhukekIvan@yandex.ru>
2022-03-08 03:06:11 +0300

    ready

--- src/components/util.js
+++ src/components/util.js
@@ -11,31 +11,5 @@ export function closePopup() {
   popupOpened.classList.remove('popup_opened');
   popupOpened.removeEventListener('click', closePopupOverlay);
   document.removeEventListener('keydown', closePopupEsc);
+  popupOpened.querySelector('.popup__form').reset();
 };   /* Функция закрывает открытый попап */
-
-export const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
